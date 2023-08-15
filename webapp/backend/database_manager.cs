4befe176a20586bf73fc47dfcb3e7f8eddd201bc
Ajay Ravji <22358465@uict.nz>
2023-08-15 15:04:47 +1200

    feat: view, add, delete courses

---
+++ webapp/backend/database_manager.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+class DatabaseContext : DbContext {
+    public DbSet<Course> Courses { get; set; }
+
+    public DatabaseContext() {}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseSqlite($"Data Source=" + Environment.CurrentDirectory + "\\database.db");
+    }
+}
+

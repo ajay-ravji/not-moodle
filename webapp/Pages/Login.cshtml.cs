17806434b53b468a8e33ff2b084bde0615a47699
Ajay Ravji <22358465@uict.nz>
2023-08-29 14:39:38 +1200

    feat: login system, minor fixes

---
+++ webapp/Pages/Login.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages;
+
+public class LoginModel : PageModel {
+    private readonly ILogger<LoginModel> _logger;
+
+    public LoginModel(ILogger<LoginModel> logger) {
+        _logger = logger;
+    }
+
+    public void OnGet() {
+        
+    }
+}
---
+++ webapp/backend/UserSession.cs
@@ -0,0 +1,4 @@
+public class UserSession {
+    public string UserName { get; set; }
+    public string Role { get; set; }
+};

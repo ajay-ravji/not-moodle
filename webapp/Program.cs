8a769fd5569a37865c74294923e0206cbd1e53a8
Ajay Ravji <22358465@uict.nz>
2023-09-12 13:30:21 +1200

    temp

--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,6 +1,13 @@
 using System.Collections.Specialized;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using System.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -8,8 +15,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -50,6 +56,37 @@ app.MapDelete("/api/course/delete", (HttpRequest request) => {
     Course course = context.Courses.Where(x => x.CourseId == courseId).ToList()[0];
     context.Courses.Remove(course);
     context.SaveChanges();
+});
+
+app.MapPost("/api/auth/login", async (HttpContext context, HttpRequest request) => {
+    var body = new StreamReader(request.Body);
+    var json = JsonSerializer.Deserialize<Dictionary<string, string>>(await body.ReadToEndAsync());
+
+    DatabaseContext dbContext = new DatabaseContext();
+    var users = dbContext.Users.Where(x => x.UserName == json["username"]).ToList();
+    if (users.Count == 0) return Results.StatusCode(400);
+
+    User user = users[0];
+    if (user.Password != json["password"]) return Results.StatusCode(400);
+
+    var claims = new List<Claim>{
+        new Claim("id", user.UserId.ToString()),
+        new Claim(ClaimTypes.Name, "test name")
+    };
+
+    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+    await context.SignInAsync(
+        CookieAuthenticationDefaults.AuthenticationScheme,
+        new ClaimsPrincipal(claimsIdentity),
+        new AuthenticationProperties{}
+    );
+    return Results.StatusCode(200);
+});
+
+app.MapPost("/api/auth/logout", async (HttpContext context, HttpRequest request) => {
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.StatusCode(200);
 });
 
 
---
+++ webapp/backend/data/user.cs
@@ -0,0 +1,6 @@
+public class User {
+    public int UserId { get; set; }
+    public string Role { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
